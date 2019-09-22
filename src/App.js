4116b480fa31362033f571140227a371c07e4f81
Jonathan Choi <jisung594@gmail.com>
2019-09-22 17:13:15 -0400

    build folder ready

--- src/App.js
+++ src/App.js
@@ -36,9 +36,7 @@ class App extends Component {
   callBackend = () => {
     fetch('/api/elements')
       // .then(res => res.json())
-      .then(data => this.setState({
-        elements: data
-      }))
+      .then(data => console.log(data))
   }
 
   mouseHandler = (e, elementObj) => {
